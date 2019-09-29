70fe33213d496e1f0df303b186e95f1f4d01e97b
Yaojin Sun <sunyaojin2011@foxmail.com>
2019-09-28 22:37:21 -0400

    done

--- show_animation_effects.js
+++ show_animation_effects.js
@@ -1,11 +1,11 @@
 var Tone = require("tone");
 const synth = new Tone.MembraneSynth().toMaster();
-//var synth2 = new Tone.FMSynth().toMaster();
-var synth2 = new Tone.PolySynth(6, Tone.Synth, {
-    oscillator : {
-        type : "square"
-    }
-}).toMaster();
+var synth2 = new Tone.FMSynth().toMaster();
+// var synth2 = new Tone.PolySynth(6, Tone.Synth, {
+//     oscillator : {
+//         type : "square"
+//     }
+// }).toMaster();
 
 
 
@@ -17,10 +17,11 @@ var map = new mapboxgl.Map({
     zoom: 0.5
 });
 
-var radius = 20;
 let max_time_interval = 2000;
+let max_time_interval_opacity = 1000;
 let all_msg_flow = [];
-
+let all_msg_flow_opacity = [];
+let init_radius = 1;
 
 let tic = 0;
 
@@ -153,33 +154,110 @@ function get_updated_marker_coordinates(src, dst, start_time, current_time){
 function extracted(timestamp_int) {
     // synth2.triggerAttackRelease('C4', '2n');
     // synth.triggerAttackRelease(["C4", "E4", "A4"], "4n");
-    let ind = tic % 7;
+    let ind = Math.floor((tic + Math.random()*10)) % 15;
 
     if ((timestamp_int * timestamp_int) % 19 == 0){
         switch (ind) {
             case 0:
-                synth.triggerAttackRelease('C4', '2n');
+                synth.triggerAttackRelease('C3', '3n');
                 break;
             case 1:
-                synth.triggerAttackRelease('Eb4', '2n');
+                synth.triggerAttackRelease('c4', '2n');
                 break;
             case 2:
-                synth.triggerAttackRelease('g4', '2n');
+                synth.triggerAttackRelease('c5', '2n');
                 break;
             case 3:
-                synth.triggerAttackRelease('C5', '2n');
+                synth.triggerAttackRelease('eb3', '2n');
                 break;
             case 4:
-                synth.triggerAttackRelease('C3', '2n');
+                synth.triggerAttackRelease('eb4', '2n');
                 break;
             case 5:
-                synth.triggerAttackRelease('F4', '2n');
+                synth.triggerAttackRelease('eb5', '2n');
                 break;
             case 6:
-                synth.triggerAttackRelease('F5', '2n');
+                synth.triggerAttackRelease('F3', '2n');
+                break;
+            case 7:
+                synth.triggerAttackRelease('f4', '2n');
+                break;
+            case 8:
+                synth.triggerAttackRelease('f5', '2n');
+                break;
+            case 9:
+                synth.triggerAttackRelease('g3', '2n');
+                break;
+            case 10:
+                synth.triggerAttackRelease('g4', '2n');
+                break;
+            case 11:
+                synth.triggerAttackRelease('g5', '2n');
+                break;
+            case 12:
+                synth.triggerAttackRelease('bb3', '2n');
+                break;
+            case 13:
+                synth.triggerAttackRelease('bb4', '2n');
                 break;
             default:
-                synth2.triggerAttackRelease('bb4', '2n');
+                synth2.triggerAttackRelease('bb5', '2n');
+        }
+        ++tic;
+    }
+}
+
+function extracted_opacity(timestamp_int) {
+    // synth2.triggerAttackRelease('C4', '2n');
+    // synth.triggerAttackRelease(["C4", "E4", "A4"], "4n");
+    let ind = Math.floor((tic + Math.random()*10)) % 15;
+
+    if ((timestamp_int * timestamp_int) % 21 == 0){
+        switch (ind) {
+            case 0:
+                synth2.triggerAttackRelease('C4', '1n');
+                break;
+            case 1:
+                synth2.triggerAttackRelease('C3', '1n');
+                break;
+            case 2:
+                synth2.triggerAttackRelease('C5', '1n');
+                break;
+            case 3:
+                synth2.triggerAttackRelease('eb3', '1n');
+                break;
+            case 4:
+                synth2.triggerAttackRelease('eb4', '1n');
+                break;
+            case 5:
+                synth2.triggerAttackRelease('eb3', '1n');
+                break;
+            case 6:
+                synth2.triggerAttackRelease('eb5', '1n');
+                break;
+            case 7:
+                synth2.triggerAttackRelease('eb4', '1n');
+                break;
+            case 8:
+                synth2.triggerAttackRelease('g4', '1n');
+                break;
+            case 9:
+                synth2.triggerAttackRelease('g5', '1n');
+                break;
+            case 11:
+                synth2.triggerAttackRelease('g3', '1n');
+                break;
+            case 12:
+                synth2.triggerAttackRelease('bb3', '1n');
+                break;
+            case 13:
+                synth2.triggerAttackRelease('bb4', '1n');
+                break;
+            case 14:
+                synth2.triggerAttackRelease('bb5', '1n');
+                break;
+            default:
+                synth2.triggerAttackRelease('bb4', '1n');
         }
         ++tic;
     }
@@ -193,8 +271,12 @@ map.on('load', function () {
         "features": []
     };
 
+    let rec_points_opcaity = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+
     console.log("---> 1");
-    console.log(rec_points);
 
     map.addSource('Mpoint', {
         "type": "geojson",
@@ -213,6 +295,81 @@ map.on('load', function () {
         "filter": ["==", "$type", "Point"],
     });
 
+    // opacity
+    map.addSource('OpacityPoint', {
+        "type": "geojson",
+        "data": rec_points_opcaity
+    });
+
+    map.addLayer({
+        "id": "opac_point",
+        "source": "OpacityPoint",
+        "type": "circle",
+        "paint": {
+            "circle-radius": init_radius,
+            "circle-color": "#D85F7D",
+            "circle-opacity": 0.8
+        },
+        "filter": ["==", "$type", "Point"],
+    });
+
+
+
+
+    // rm layer first, then remove source
+
+    function animate_opacity(timestamp) {
+        // Update the data to a new position based on the animation timestamp. The
+        // divisor in the expression `timestamp / 1000` controls the animation speed.
+        rec_points_opcaity = {
+            "type": "FeatureCollection",
+            "features": []
+        };
+
+        let timestamp_int = Math.floor(timestamp+111);
+        if (timestamp_int % 5 == 0){
+            let rand_ind1 = 5 + Math.floor(Math.random() * 40);
+            let new_ele = [];
+            new_ele.push(cities[rand_ind1]);
+            let rand_ind2 = Math.floor(Math.random() * cities.length);
+            new_ele.push([-43.277548, -22.875113]);
+            new_ele.push(timestamp_int);
+            all_msg_flow_opacity.push(new_ele);
+            console.log("added new src/dst, the length is ", all_msg_flow_opacity.length);
+        }
+
+        let should_be_updated = [];
+        for (let k=0; k<all_msg_flow_opacity.length; ++k){
+            if (timestamp_int - all_msg_flow_opacity[k][2] < max_time_interval){
+                should_be_updated.push(all_msg_flow_opacity[k]);
+            }
+        }
+        all_msg_flow_opacity = should_be_updated;
+        console.log("opacity --> ", all_msg_flow_opacity.length);
+
+        for (let i=0; i<all_msg_flow_opacity.length; ++i){
+            rec_points_opcaity.features.push(
+                get_updated_marker_coordinates(all_msg_flow_opacity[i][0], all_msg_flow_opacity[i][1], all_msg_flow_opacity[i][2], timestamp_int));
+        }
+
+        //
+        map.getSource('OpacityPoint').setData(rec_points_opcaity);
+        map.setPaintProperty('opac_point', 'circle-radius', init_radius * (1 + 2*(timestamp_int % max_time_interval)/max_time_interval));
+
+        if (all_msg_flow_opacity.length > 10){
+            extracted_opacity(timestamp_int);
+        }
+        // Request the next frame of the animation.
+        requestAnimationFrame(animate_opacity);
+    }
+
+
+
+
+
+    // simple update the data
+
+
     function animateMarker(timestamp) {
         // Update the data to a new position based on the animation timestamp. The
         // divisor in the expression `timestamp / 1000` controls the animation speed.
@@ -223,7 +380,7 @@ map.on('load', function () {
 
         let timestamp_int = Math.floor(timestamp);
         if (timestamp_int % 5 == 0){
-            let rand_ind1 = Math.floor(Math.random() * cities.length);
+            let rand_ind1 = Math.floor(Math.random() * cities.length /2);
             let new_ele = [];
             new_ele.push(cities[rand_ind1]);
             let rand_ind2 = Math.floor(Math.random() * cities.length);
@@ -262,6 +419,8 @@ map.on('load', function () {
 
     // Start the animation.
     animateMarker(0);
+    animate_opacity(0);
+
 });
 
 
